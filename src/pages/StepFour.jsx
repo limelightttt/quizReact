f8d5be7ce92ec74949ebd54fb017d94f7784a11d
unknown <roman150803@gmail.com>
2025-02-28 21:33:17 +0500

    readyy

--- src/pages/StepFour.jsx
+++ src/pages/StepFour.jsx
@@ -1,51 +1,67 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
+import { AppButton } from "../components/AppButton";
+import { Header } from "../components/Header";
+import { ProgressBar } from "../components/ProgressBar";
+import { useNavigate } from "react-router-dom";
 
 const StepFour = () => {
+  const [points, setPoints] = useState("");
+  const navigate = useNavigate();
+  const variant = [
+    {
+      id: "variant-1",
+      points: 1,
+    },
+    {
+      id: "variant-2",
+      points: 2,
+    },
+    {
+      id: "variant-3",
+      points: 3,
+    },
+    {
+      id: "variant-4",
+      points: 4,
+    },
+    {
+      id: "variant-5",
+      points: 5,
+    },
+  ];
+  useEffect(() => {
+    const userData = JSON.parse(localStorage.getItem("user")) || {};
+    localStorage.setItem("user", JSON.stringify({ ...userData, points }));
+  }, [points]);
+
   return (
     <div className="container">
       <div className="wrapper">
         <div className="emoji-quiz">
-          <div className="indicator">
-            <div className="indicator__text">
-              <span className="indicator__description">
-                Скидка за прохождение опроса:
-              </span>
-              <span className="indicator__value">15%</span>
-            </div>
-            <div className="indicator__progressbar">
-              <div className="indicator__unit indicator__unit-1 _active"></div>
-              <div className="indicator__unit indicator__unit-2 _active"></div>
-              <div className="indicator__unit indicator__unit-3 _active"></div>
-              <div className="indicator__unit indicator__unit-4"></div>
-            </div>
-          </div>
+          <ProgressBar currentStep={4} />
           <div className="question">
-            <h2>4. Занимательный вопрос</h2>
+            <Header headerText="4. Оцените наш учебный центр" headerType="h2" />
             <ul className="level-variants">
-              <li className="variant-wrapper">
-                <input required type="radio" name="variant" id="variant-1" />
-                <label htmlFor="variant-1">1</label>
-              </li>
-              <li className="variant-wrapper">
-                <input required type="radio" name="variant" id="variant-2" />
-                <label htmlFor="variant-2">2</label>
-              </li>
-              <li className="variant-wrapper">
-                <input required type="radio" name="variant" id="variant-3" />
-                <label htmlFor="variant-3">3</label>
-              </li>
-              <li className="variant-wrapper">
-                <input required type="radio" name="variant" id="variant-4" />
-                <label htmlFor="variant-4">4</label>
-              </li>
-              <li className="variant-wrapper">
-                <input required type="radio" name="variant" id="variant-5" />
-                <label htmlFor="variant-5">5</label>
-              </li>
+              {variant.map((elem) => (
+                <li className="variant-wrapper" key={elem.id}>
+                  <input
+                    value={elem.points}
+                    required
+                    type="radio"
+                    name="variant"
+                    id={elem.id}
+                    onChange={(e) => {
+                    setPoints(e.target.value);
+                    }}
+                  />
+
+                  <label htmlFor={elem.id}>{elem.points}</label>
+                </li>
+              ))}
             </ul>
-            <button type="button" id="next-btn" disabled>
-              Далее
-            </button>
+            <AppButton 
+            isDisabled={points ? false : true} 
+            btnClick={() => navigate("/thanks")}/>
           </div>
         </div>
       </div>
--- src/pages/Thanks.jsx
+++ src/pages/Thanks.jsx
@@ -1,12 +1,49 @@
 import React from "react";
+import { Header } from "../components/Header";
 
 const Thanks = () => {
+  const { name, phone, answer, selectCourse, selectEmoji, points } = 
+  JSON.parse(localStorage.getItem("user"));
+  const value = [
+    {
+      text: "Имя",
+      userResponse: name,
+    },
+    {
+      text: "Номер телефона",
+      userResponse: phone,
+    },
+    {
+      text: "Откуда узнали",
+      userResponse: answer,
+    },
+    {
+      text: "Желаемый курс",
+      userResponse: selectCourse,
+    },
+    {
+      text: "Ваш характер",
+      userResponse: selectEmoji,
+    },
+    {
+      text: "Оценка школы",
+      userResponse: points,
+    },
+  ];
   return (
     <div className="container">
       <div className="wrapper">
         <div className="thanks">
           <img src="./img/bell.png" alt="bell" />
-          <h1>Спасибо за прохождение опроса!</h1>
+          <Header headerText="Спасибо за прохождение опроса!" headerType="h1" />
+          <div className="infoWrapper">
+          {value.map((elem) => (
+            <div className="info">
+            <span>{elem.text}</span>
+            <span>{elem.userResponse}</span>
+          </div>
+          ))}
+          </div>
           <p>Получи свою скидку по ссылке ниже или другое блаблабла</p>
           <button type="button" id="get-link">
             Получить ссылку
